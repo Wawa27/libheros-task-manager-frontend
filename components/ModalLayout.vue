a96a79e0192b1d0606ab814ba319dba889ee4e72
Wawa27 <32011804+Wawa27@users.noreply.github.com>
2024-12-20 07:38:24 +0100

    feat: add login (#2)

---
+++ components/ModalLayout.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import {useModalStore} from '~/stores/modal';
+
+const modalStore = useModalStore();
+</script>
+
+<template>
+  <div
+      v-if="modalStore.state.component"
+      id="modal-layout"
+      @click.self="modalStore.close"
+  >
+    <!-- Assign component state && component props -->
+    <Component
+        :is="modalStore.state.component"
+        v-bind="modalStore.state.componentProps"
+        @close="modalStore.close"
+    />
+  </div>
+</template>
+
+<style scoped>
+#modal-layout {
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgb(0, 0, 0);
+  background-color: rgba(0, 0, 0, 0.4);
+}
+</style>
---
+++ stores/auth.ts
@@ -0,0 +1,36 @@
+import {defineStore} from 'pinia';
+import axios from "~/services/axios";
+
+export const useAuthStore = defineStore('auth', {
+    state: () => ({
+        token: null as string | null,
+    }),
+    actions: {
+        async login(email: string, password: string) {
+            try {
+                const response = await axios.post('/auth/login', {email, password});
+                this.token = response.data.access_token;
+                localStorage.setItem('accessToken', this.token);
+                this.setAxiosToken(this.token);
+            } catch (error) {
+                console.error('Login failed:', error);
+                throw error;
+            }
+        },
+        setAxiosToken(token: string | null) {
+            axios.defaults.headers.common['Authorization'] = token ? `Bearer ${token}` : '';
+        },
+        logout() {
+            this.token = null;
+            localStorage.removeItem('accessToken');
+            this.setAxiosToken(null);
+        },
+        loadTokenFromStorage() {
+            const token = localStorage.getItem('accessToken');
+            if (token) {
+                this.token = token;
+                this.setAxiosToken(token);
+            }
+        },
+    },
+});
